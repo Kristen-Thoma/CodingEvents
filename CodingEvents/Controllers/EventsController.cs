13d757a09371bb62593cd302cf47bf1babbfe5fb
Kristen Thoma <kristenthoma@gmail.com>
2021-07-23 18:04:05 -0400

    commit

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CodingEvents.Data;
+using CodingEvents.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,17 +10,14 @@ namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
-        static private Dictionary<string, string> Events = new();
 
-        [HttpGet]
         public IActionResult Index()
         {
-            ViewBag.events = Events;
+            ViewBag.events = EventData.GetAll();
 
             return View();
         }
 
-        [HttpGet]
         public IActionResult Add()
         {
             return View();
@@ -26,10 +25,48 @@ namespace CodingEvents.Controllers
 
         [HttpPost]
         [Route("/Events/Add")]
-        public IActionResult NewEvent(string name, string description)
+        public IActionResult NewEvent(Event newEvent)
         {
-            Events.Add(name, description);
+            EventData.Add(newEvent);
+
             return Redirect("/Events");
+        }
+
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach (int eventId in eventIds)
+            {
+                EventData.Remove(eventId);
+            }
+
+            return Redirect("/Events");
+        }
+
+        [Route("/Events/Edit/{eventId}")] //Question 3
+        public IActionResult Edit(int eventId) //Question 1.a
+        {
+            Event editEvent = EventData.GetById(eventId); //Question 6.a
+            ViewBag.eventToEdit = editEvent; //Question 6.b
+            ViewBag.title = $"Edit Event {editEvent.Name} {editEvent.Id}"; //Question 9
+            return View(); //Question 6.c
+        }
+
+        [HttpPost]
+        [Route("/Events/Edit")]  //Question 2 
+        public IActionResult SubmitEditEventForm(int eventId, string name, string description) //Question 1.b
+        {
+            Event eventToEdit = EventData.GetById(eventId); //Question 10.a
+            eventToEdit.Name = name; //Question 10.b
+            eventToEdit.Description = description; //Question 10.b
+            return Redirect("/Events"); //Question 10.c
         }
     }
 }
